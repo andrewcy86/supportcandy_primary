2c551e1cbaf2cff2dbbdea080298d40abd145c02
Andrew Yuen <yuen.andrew@epa.gov>
2021-03-31 22:43:58 -0400

    v 2.1.7.18

--- includes/admin/tickets/individual_ticket/get_change_ticket_status.php
+++ includes/admin/tickets/individual_ticket/get_change_ticket_status.php
@@ -314,7 +314,7 @@ jQuery(".wpsc_popup_action").click(function () {
 if(request_status == <?php echo $new_request_tag->term_id; ?> || request_status == <?php echo $initial_review_rejected_tag->term_id; ?> || request_status == <?php echo $cancelled_tag->term_id; ?>) {
     alert('No automatic shelf assignments made.');
 } else {
-    alert(jQuery("[name=category]").val());
+
 jQuery.post(
 '<?php echo WPPATT_PLUGIN_URL; ?>includes/admin/pages/scripts/auto_assignment.php',{
 postvartktid: '<?php echo $ticket_id ?>',
